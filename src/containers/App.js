0dc49322c3d07044c3d1ea0ce8ccca3ef6bb9c62
tupekm <marintupek@gmail.com>
2019-11-11 11:05:54 +0000

    React redux implemented - updating

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,72 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/searchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
+
 import '../containers/App.css';
 
+import { setSearchField, requestRobots} from '../actions';
+
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
-    constructor(){
-        super()
-        this.state ={
-            robots : [],
-            searchfield: ''
-        }
-    }
+
+    // constructor(){
+    //     super()
+    //     this.state ={
+    //         robots : []
+    //         // searchfield: ''
+    //     }
+    //}
 
     componentDidMount(){
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => {this.setState({robots: users})});
+        this.props.onRequestRobots();
+        // fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => response.json())
+        // .then(users => {this.setState({robots: users})});
         //this.setState({robots: name})
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
+    // onSearchChange = (event) => {
+    //      this.setState({searchfield: event.target.value})
         
         
-    }
+    // }
     render(){
-        const {robots, searchfield} = this.state;
+        // const {robots} = this.state;
+        const {searchField, onSearchChange, robots, isPending} = this.props;
         const filteredRobots = robots.filter(robots => {
-            return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robots.name.toLowerCase().includes(searchField.toLowerCase())
         })
-        if (this.state.robots.lenght === 0){
-            return <h1>Loading</h1>
-        }else{
-
-            return (
+            return isPending ?
+             <h1>Loading</h1>:
+            
+             (
                 <div className= 'tc'>
                     <h1 className = 'f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                     <Scroll>
-                        <ErrorBoundry>
                             <CardList robots = {filteredRobots}/>
-                            </ErrorBoundry>
-                    </Scroll>
+                     </Scroll>
                  </div>
                 );
             }
     }
-}
-export default App;
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import { createLogger} from 'redux-logger';
+import thunkMiddleWare from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
+import { applyMiddleware } from 'redux';
+import { combineReducers} from 'redux';
 
+const logger = createLogger();
 
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleWare, logger))
 
-ReactDOM.render(<App/>, document.getElementById('root'));
-
+ReactDOM.render(
+    <Provider store = {store}>
+        <App/>
+    </Provider>, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
